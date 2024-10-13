488ad2e9202e0c2557dcacd313a3e8c52b5c16bd
Timur Kazdayev <tkazdayev@gmail.com>
2024-10-13 21:19:19 +0500

    update

---
+++ django_docker_project/myproject/settings.py
@@ -0,0 +1,23 @@
+import os
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': 5432,
+    }
+}
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'blog',
+]
